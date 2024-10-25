bf845e52f82fcff90cdf83e85921f70adbaaa397
SkyZYNE <81781579+SkyZYNE@users.noreply.github.com>
2024-10-25 22:21:02 +0700

    Update delete_task.php

--- delete_task.php
+++ delete_task.php
@@ -5,10 +5,8 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $task_id = (int)$_POST['task_id'];
 
-    // Hapus task dari database berdasarkan task_id saja
     $stmt = $pdo->prepare("DELETE FROM tasks WHERE id = :task_id");
     $stmt->execute(['task_id' => $task_id]);
 
-    // Redirect ke halaman sebelumnya (view_todo.php) setelah task dihapus
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
